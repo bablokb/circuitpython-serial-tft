1a15fffd16183e421695bc80376b5cb7f3a5574b
Bernhard Bablok <bablokb@gmx.de>
2025-11-13 20:25:13 +0100

    add predefined colors module

---
+++ serial_tft/colors.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------
+# Driver for the OpenSmart 2.4" Serial-TFT
+#
+# Some predefined colors
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/circuitpython-serial-tft
+# ----------------------------------------------------------------------------
+
+BLACK   = 0x0000
+BLUE    = 0x001F
+RED     = 0xF800
+GREEN   = 0x07E0
+CYAN    = 0x07FF
+MAGENTA = 0xF81F
+YELLOW  = 0xFFE0
+WHITE   = 0xFFFF
